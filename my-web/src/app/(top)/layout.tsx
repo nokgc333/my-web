6c2b5c7f9841e35908aebb5f9e8337228e6ae883
Naoki Kaneko <nokgc333@gmail.com>
2026-02-07 19:22:44 +0900

    feat: ヘッダー作成

---
+++ my-web/src/app/(top)/layout.tsx
@@ -0,0 +1,28 @@
+import { buttonVariants } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+export default function TopLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {/* headerコンポーネントでも良い */}
+      <header className="container z-40 bg-background">
+        <div className="h-20 py-6">
+          <nav>
+            <Link
+              href={'/login'}
+              className={cn(
+                buttonVariants({ variant: 'secondary', size: 'sm' }),
+                'px-4'
+              )}
+            >
+              ログイン
+            </Link>
+          </nav>
+        </div>
+      </header>
+
+      <main>{children}</main>
+    </div>
+  );
+}
