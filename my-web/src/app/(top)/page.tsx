82c7dde281417ac78e6bc11d838a4cfd3c094bf1
Naoki Kaneko <nokgc333@gmail.com>
2026-02-08 19:30:26 +0900

    feat: featuresセクション作成

--- my-web/src/app/(top)/page.tsx
+++ my-web/src/app/(top)/page.tsx
@@ -50,6 +50,21 @@ export default function TopPage() {
             </div>
           </div>
         </section>
+
+        <section
+          id="features"
+          className="container py-8 md:py-12 lg:py-24 bg-slate-50"
+        >
+          <div className="text-center max-w-[58rem] mx-auto space-y-6">
+            <h2 className="text-3xl md:text-6xl font-extrabold">about</h2>
+            <p className="sm:text-lg sm:leading-7 text-muted-foreground">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum
+              provident architecto dolor iusto, odit perferendis odio laboriosam
+              dolorem molestiae nulla reprehenderit doloribus repellat rem
+              labore quaerat ipsum? Dolore, molestiae animi!
+            </p>
+          </div>
+        </section>
       </main>
     </>
   );
