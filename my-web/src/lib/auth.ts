4992ccb5e4acf8f556f64b3c505c3422253831cb
Naoki Kaneko <nokgc333@gmail.com>
2026-02-15 20:25:55 +0900

    chore: Prismaマイグレーション操作でモデルをテーブルへ反映

--- my-web/src/lib/auth.ts
+++ my-web/src/lib/auth.ts
@@ -10,10 +10,12 @@ export const authOptions: NextAuthOptions = {
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   adapter: PrismaAdapter(db),
